c679c889fa2e5578868c717b389312cdbdc35615
13000018 <13000018@e010m03.istic.univ-rennes1.fr>
2014-02-26 10:10:09 +0100

    Devinette

---
+++ JavaSource/fr/ifsic/tpj2ee/NumberBean.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package fr.ifsic.tpj2ee;
+
+/**
+ * @author 13000018
+ *
+ */
+public class NumberBean {
+	private int userNumber;
+	private int randomNumber;
+
+	public NumberBean() {
+	}
+
+	public int getUserNumber() {
+		return userNumber;
+	}
+
+	public void setUserNumber(int userNumber) {
+		this.userNumber = userNumber;
+	}
+
+	public int getRandomNumber() {
+		return randomNumber;
+	}
+
+	public void setRandomNumber(int randomNumber) {
+		this.randomNumber = randomNumber;
+	}
+	
+	
+	
+	
+	
+	public void soumettre(){
+		
+	}
+	public void playAgain(){
+		
+	}
+	
+}
---
+++ WebContent/WEB-INF/index.jsp
@@ -0,0 +1,5 @@
+<html>
+ <body>
+  <jsp:forward page="/pages/invite.jsf" />
+ </body>
+</html>
